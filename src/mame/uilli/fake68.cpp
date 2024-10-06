82c458b3ae5c66f7c00fa5615e5141d22f9a1905
Antonio "Willy" Malara <antonio.malara@gmail.com>
2024-10-06 20:01:50 +0200

    add the driver

---
+++ src/mame/uilli/fake68.cpp
@@ -0,0 +1,327 @@
+// license:BSD-3-Clause
+
+// based on https://www.retrobrewcomputers.org/doku.php?id=builderpages:plasmo:cb030:cb030_rev1:cb030r1_memmap
+// in order to boot a build of https://github.com/John-Titor/os9-m68k-ports/tree/main/ports/CB030
+
+/*
+
+debugger snippets:
+
+prints init module ptr
+print d@(d@0 + 0x20) 
+
+module dir start
+print d@(d@(d@0 + 0x20) + $03C)
+
+module dir end:
+print d@(d@(d@0 + 0x20) + $03C)
+
+*/
+
+#include "emu.h"
+#include "cpu/m68000/m68010.h"
+#include "cpu/m68000/m68020.h"
+#include "cpu/m68000/m68030.h"
+#include "machine/mc68681.h"
+#include "machine/terminal.h"
+#include "bus/rs232/rs232.h"
+#include "bus/ata/ataintf.h"
+#include "bus/ata/hdd.h"
+#include "video/pwm.h"
+
+#include <stdio.h>
+#include <signal.h>
+
+namespace {
+
+class fake68_state : public driver_device
+{
+public:
+    fake68_state(const machine_config &mconfig, device_type type, const char *tag)
+        : driver_device(mconfig, type, tag)
+        , m_maincpu(*this, "maincpu")
+        , m_duart(*this, "duart")
+        , m_rs232_a(*this, "rs232_a")
+        , m_rs232_b(*this, "rs232_b")
+		, m_ata(*this, "ata")
+        , m_display(*this, "display")
+    {
+    }
+
+    void fake68(machine_config &config);
+
+protected:
+    virtual void machine_reset() override;
+    virtual void machine_start() override;
+
+private:
+    required_device<cpu_device> m_maincpu;
+    required_device<mc68681_device> m_duart;
+    required_device<rs232_port_device> m_rs232_a;
+    required_device<rs232_port_device> m_rs232_b;
+	required_device<ata_interface_device> m_ata;
+    required_device<pwm_display_device> m_display;
+
+    void main_map(address_map &map);
+    void cpu_map(address_map &map);
+
+    void duart_irq_handler(int state);
+    void duart_output(uint8_t data);
+
+    emu_timer *m_tick_timer;
+    TIMER_CALLBACK_MEMBER(tick_timer);
+
+    uint32_t bootvect_r(offs_t offset);
+    void bootvect_w(offs_t offset, uint32_t data, uint32_t mem_mask);
+
+    uint32_t remap_r(offs_t offset);
+    void remap_w(offs_t offset, uint32_t data, uint32_t mem_mask);
+
+    uint8_t mc68681_read(offs_t offset);
+    void mc68681_write(offs_t offset, uint8_t data);
+
+    uint32_t timer_on_r(offs_t offset);
+    void timer_on_w(offs_t offset, uint32_t data, uint32_t mem_mask);
+
+    uint32_t timer_off_r(offs_t offset);
+    void timer_off_w(offs_t offset, uint32_t data, uint32_t mem_mask);
+
+    bool m_did_bootvect_hack;
+    uint32_t m_bootvect_hack[2];
+
+    uint8_t cf_r(offs_t offset);
+    void cf_w(offs_t offset, uint8_t data);
+};
+
+void fake68_state::machine_reset() 
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+    // raise(SIGTRAP);
+
+    m_duart->ip0_w(1);
+    m_duart->ip1_w(1);
+    m_duart->ip2_w(1);
+    m_duart->ip3_w(1);
+    m_duart->ip4_w(1);
+    m_duart->ip5_w(1);
+    m_duart->ip6_w(1);
+}
+
+void fake68_state::machine_start()
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+    m_did_bootvect_hack = false;
+    m_tick_timer = timer_alloc(FUNC(fake68_state::tick_timer), this);
+    m_tick_timer->adjust(attotime::from_hz(100), 0, attotime::from_hz(100));
+    m_tick_timer->enable(false);
+}
+
+uint8_t fake68_state::mc68681_read(offs_t offset)
+{
+    return m_duart->read(offset >> 1);
+}
+
+void fake68_state::mc68681_write(offs_t offset, uint8_t data)
+{
+    m_duart->write(offset >> 1, data);
+}
+
+void fake68_state::main_map(address_map &map)
+{
+    map(0x00000000, 0x00000007).rw(FUNC(fake68_state::bootvect_r), FUNC(fake68_state::bootvect_w)),
+    map(0x00000008, 0x07ffffff).ram();
+    map(0xfe000000, 0xfeffffff).rom().region("eprom", 0);
+    map(0xffff8000, 0xffff8fff).rw(FUNC(fake68_state::remap_r), FUNC(fake68_state::remap_w)),
+    map(0xffff9800, 0xffff9fff).rw(FUNC(fake68_state::timer_on_r), FUNC(fake68_state::timer_on_w)),
+    map(0xffff9000, 0xffff97ff).rw(FUNC(fake68_state::timer_off_r), FUNC(fake68_state::timer_off_w)),
+    map(0xffffe000, 0xffffefff).rw(FUNC(fake68_state::cf_r), FUNC(fake68_state::cf_w)),
+    map(0xfffff000, 0xffffffff).rw(FUNC(fake68_state::mc68681_read), FUNC(fake68_state::mc68681_write));
+}
+
+void fake68_state::cpu_map(address_map &map)
+{
+    map(0xfffffff3, 0xfffffff3).lr8(NAME([]() { printf("avec 1"); return m68000_base_device::autovector(1); }));
+    map(0xfffffff5, 0xfffffff5).lr8(NAME([]() { printf("avec 2"); return m68000_base_device::autovector(2); }));
+    map(0xfffffff7, 0xfffffff7).lr8(NAME([]() { printf("avec 3"); return m68000_base_device::autovector(3); }));
+    map(0xfffffff9, 0xfffffff9).lr8(NAME([]() { printf("avec 4"); return m68000_base_device::autovector(4); }));
+    map(0xfffffffb, 0xfffffffb).lr8(NAME([]() { printf("avec 5"); return m68000_base_device::autovector(5); }));
+    map(0xfffffffd, 0xfffffffd).lr8(NAME([]() { printf("avec 6"); return m68000_base_device::autovector(6); }));
+    map(0xffffffff, 0xffffffff).lr8(NAME([]() { printf("avec 7"); return m68000_base_device::autovector(7); }));
+}
+
+uint32_t fake68_state::bootvect_r(offs_t offset)
+{
+    static const uint32_t bootvect_pc[] = {
+        0x00000000,
+        0xfe000494,
+    };
+
+    if (!m_did_bootvect_hack)
+        return bootvect_pc[offset];
+     else
+        return m_bootvect_hack[offset];
+}
+
+void fake68_state::bootvect_w(offs_t offset, uint32_t data, uint32_t mem_mask)
+{
+    m_bootvect_hack[offset % sizeof(m_bootvect_hack)] &= ~mem_mask;
+    m_bootvect_hack[offset % sizeof(m_bootvect_hack)] |= (data & mem_mask);
+}
+
+uint32_t fake68_state::remap_r(offs_t offset)
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+    m_did_bootvect_hack = true;
+    return 0;
+}
+
+void fake68_state::remap_w(offs_t offset, uint32_t data, uint32_t mem_mask)
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+    m_did_bootvect_hack = true;
+}
+
+uint32_t fake68_state::timer_on_r(offs_t offset)
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+    m_tick_timer->enable(true);
+    return 0;
+}
+
+void fake68_state::timer_on_w(offs_t offset, uint32_t data, uint32_t mem_mask)
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+    m_tick_timer->enable(true);
+}
+
+uint32_t fake68_state::timer_off_r(offs_t offset)
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+    m_tick_timer->enable(false);
+    return 0;
+}
+
+void fake68_state::timer_off_w(offs_t offset, uint32_t data, uint32_t mem_mask)
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+    m_tick_timer->enable(false);
+}
+
+uint8_t fake68_state::cf_r(offs_t offset)
+{
+    return m_ata->cs0_r(offset, 0xff);
+}
+
+void fake68_state::cf_w(offs_t offset, uint8_t data)
+{
+    m_ata->cs0_w(offset, data, 0xff);
+}
+
+void fake68_state::duart_irq_handler(int state)
+{
+    if (state) {
+        m_maincpu->set_input_line(M68K_IRQ_2, CLEAR_LINE);
+        m_maincpu->set_input_line(M68K_IRQ_1, CLEAR_LINE);
+        m_maincpu->set_input_line(M68K_IRQ_3, ASSERT_LINE);
+    } else {
+        m_maincpu->set_input_line(M68K_IRQ_2, CLEAR_LINE);
+        m_maincpu->set_input_line(M68K_IRQ_1, CLEAR_LINE);
+        m_maincpu->set_input_line(M68K_IRQ_3, CLEAR_LINE);
+    }
+}
+
+void display_7seg(uint8_t segments) {
+    char a = (segments & (1 << 0)) ? '-' : ' ';
+    char b = (segments & (1 << 1)) ? '|' : ' ';
+    char c = (segments & (1 << 2)) ? '|' : ' ';
+    char d = (segments & (1 << 3)) ? '-' : ' ';
+    char e = (segments & (1 << 4)) ? '|' : ' ';
+    char f = (segments & (1 << 5)) ? '|' : ' ';
+    char g = (segments & (1 << 6)) ? '-' : ' ';
+
+    printf(" %c \n", a);
+    printf("%c %c\n", f, b);
+    printf(" %c \n", g);
+    printf("%c %c\n", e, c);
+    printf(" %c \n", d);
+}
+
+void fake68_state::duart_output(uint8_t data)
+{
+    printf("%s got %02x %c\n", __PRETTY_FUNCTION__, data, data);
+    m_display->write_mx(data);
+    display_7seg(data);
+}
+
+static INPUT_PORTS_START(fake68)
+INPUT_PORTS_END
+
+static DEVICE_INPUT_DEFAULTS_START(terminal_a)
+    DEVICE_INPUT_DEFAULTS( "RS232_TXBAUD", 0xff, RS232_BAUD_19200 )
+    DEVICE_INPUT_DEFAULTS( "RS232_RXBAUD", 0xff, RS232_BAUD_19200 )
+    DEVICE_INPUT_DEFAULTS( "RS232_DATABITS", 0xff, RS232_DATABITS_8 )
+    DEVICE_INPUT_DEFAULTS( "RS232_PARITY", 0xff, RS232_PARITY_NONE )
+    DEVICE_INPUT_DEFAULTS( "RS232_STOPBITS", 0xff, RS232_STOPBITS_1 )
+DEVICE_INPUT_DEFAULTS_END
+
+
+static DEVICE_INPUT_DEFAULTS_START(terminal_b)
+    DEVICE_INPUT_DEFAULTS( "RS232_TXBAUD", 0xff, RS232_BAUD_19200 )
+    DEVICE_INPUT_DEFAULTS( "RS232_RXBAUD", 0xff, RS232_BAUD_19200 )
+    DEVICE_INPUT_DEFAULTS( "RS232_DATABITS", 0xff, RS232_DATABITS_8 )
+    DEVICE_INPUT_DEFAULTS( "RS232_PARITY", 0xff, RS232_PARITY_NONE )
+    DEVICE_INPUT_DEFAULTS( "RS232_STOPBITS", 0xff, RS232_STOPBITS_1 )
+DEVICE_INPUT_DEFAULTS_END
+
+
+static void cfcard_option(device_slot_interface &device)
+{
+    device.option_add("cfcard", ATA_CF);
+}
+
+void fake68_state::fake68(machine_config &config)
+{
+    M68030(config, m_maincpu, 16_MHz_XTAL);
+    m_maincpu->set_addrmap(AS_PROGRAM, &fake68_state::main_map);
+    // m_maincpu->set_addrmap(m68030_device::AS_CPU_SPACE, &fake68_state::cpu_map);
+
+    MC68681(config, m_duart, 8_MHz_XTAL / 2);
+    m_duart->set_clocks(500000, 500000, 1000000, 1000000);
+    m_duart->irq_cb().set(FUNC(fake68_state::duart_irq_handler));
+    m_duart->outport_cb().set(FUNC(fake68_state::duart_output));
+
+    RS232_PORT(config, m_rs232_a, default_rs232_devices, "terminal");
+    m_rs232_a->set_option_device_input_defaults("terminal", DEVICE_INPUT_DEFAULTS_NAME(terminal_a));
+
+    m_duart->a_tx_cb().set(m_rs232_a, FUNC(rs232_port_device::write_txd));    
+    m_rs232_a->rxd_handler().set(m_duart, FUNC(mc68681_device::rx_a_w));
+
+    RS232_PORT(config, m_rs232_b, default_rs232_devices, "terminal");
+    m_rs232_b->set_option_device_input_defaults("terminal", DEVICE_INPUT_DEFAULTS_NAME(terminal_b));
+
+    m_duart->b_tx_cb().set(m_rs232_b, FUNC(rs232_port_device::write_txd));    
+    m_rs232_b->rxd_handler().set(m_duart, FUNC(mc68681_device::rx_b_w));
+
+    ATA_INTERFACE(config, m_ata).options(cfcard_option, "cfcard");
+
+    PWM_DISPLAY(config, m_display).set_size(6, 7);
+    m_display->set_segmask(0x3f, 0x7f);
+}
+
+
+TIMER_CALLBACK_MEMBER(fake68_state::tick_timer)
+{
+    m_maincpu->set_input_line(M68K_IRQ_6, ASSERT_LINE);
+}
+
+ROM_START(fake68)
+    ROM_REGION32_BE(0xfe000000, "eprom", 0)
+    // ROM_LOAD("romboot", 0x000000, 0x000158ac, CRC(3de4486c) SHA1(d7e86da86d1ab5b11eb856113d62c99a5e40d36c))
+    // ROM_LOAD("romimage.dev", 0x000000, 0x00061a2e, CRC(0eef4a25) SHA1(0d63281839eac90dd15b107c33443cb05bf84ba0))
+    ROM_LOAD("romimage.dev.patched-debugger", 0x00000000, 0x00070000, CRC(0eef4a25) SHA1(0d63281839eac90dd15b107c33443cb05bf84ba0))
+ROM_END
+
+}
+
+SYST(2024, fake68, 0, 0, fake68, fake68, fake68_state, empty_init, "Uilli", "Fake 68k machine", MACHINE_IS_SKELETON);
+
