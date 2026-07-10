74b5ea06bf325d428e696ba17ce807d8c134000c
Enrico Gueli <enrico.gueli@gmail.com>
2026-07-10 20:58:05 +0200

    add basic 6545 chip and screen

--- src/devices/bus/vme/aes2_crtc.h
+++ src/devices/bus/vme/aes2_crtc.h
@@ -2,6 +2,10 @@
 
 #include "bus/vme/vme.h"
 #include "bus/vme/vme_cards.h"
+#include "video/mc6845.h"
+#include "screen.h"
+#include "render.h"
+
 
 DECLARE_DEVICE_TYPE(VME_AESTHEDES2_CRTC, aesthedes2_vme_crtc_device);
 
@@ -11,6 +15,7 @@ public:
     aesthedes2_vme_crtc_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock)
         : device_t(mconfig, VME_AESTHEDES2_CRTC, tag, owner, clock)
         , device_vme_card_interface(mconfig, *this)
+        , m_crtc(*this, "crtc")
     {
     }
 
@@ -28,7 +33,29 @@ protected:
             write32_delegate(*this, FUNC(aesthedes2_vme_crtc_device::write32)));
     }
 
+    virtual void device_reset() override ATTR_COLD
+    {
+        //machine().render().target_by_index(1)->set_view(0);
+    }
+
+    virtual void device_add_mconfig(machine_config &config) override ATTR_COLD
+    {
+        screen_device &screen(SCREEN(config, "screen", SCREEN_TYPE_RASTER));
+        screen.set_raw(14.318181_MHz_XTAL, 896, 0, 640, 290, 0, 240);
+        screen.set_screen_update("crtc", FUNC(mc6845_device::screen_update));
+
+        R6545_1(config, m_crtc, 14.318181_MHz_XTAL / 8);
+        m_crtc->set_screen("screen");
+        m_crtc->set_show_border_area(false);
+        m_crtc->set_char_width(8);
+        // m_crtc->set_on_update_addr_change_callback(FUNC(coco_wpkrs_device::crtc_addr));
+        // m_crtc->set_update_row_callback(FUNC(coco_wpkrs_device::crtc_update_row));
+    }
+
+
 private:
+	required_device<r6545_1_device> m_crtc;
+
     u32 read32(address_space &space, offs_t offset, u32 mem_mask)
     {
         if (!machine().side_effects_disabled())
