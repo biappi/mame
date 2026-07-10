6fdd5fb6b478459e12d3df503aa8ece8a9b84dad
Enrico Gueli <enrico.gueli@gmail.com>
2026-07-10 22:29:46 +0200

    make base address configurable

--- src/devices/bus/vme/aes2_crtc.cpp
+++ src/devices/bus/vme/aes2_crtc.cpp
@@ -4,13 +4,16 @@ DEFINE_DEVICE_TYPE(VME_AESTHEDES2_CRTC, aesthedes2_vme_crtc_device, "aesthedes2_
 
 void aesthedes2_vme_crtc_device::device_start()
 {
+    if (m_base_addr == 0)
+        fatalerror("base address not set");
+        
     vme_space(vme::AM_09).install_readwrite_handler(
-        0x04ffa000, 0x04ffa01f,
+        m_base_addr, m_base_addr + 0x1f,
         read32_delegate(*this, FUNC(aesthedes2_vme_crtc_device::read32)),
         write32_delegate(*this, FUNC(aesthedes2_vme_crtc_device::write32)));
 
     vme_space(vme::AM_0d).install_readwrite_handler(
-        0x04ffa000, 0x04ffa01f,
+        m_base_addr, m_base_addr + 0x1f,
         read32_delegate(*this, FUNC(aesthedes2_vme_crtc_device::read32)),
         write32_delegate(*this, FUNC(aesthedes2_vme_crtc_device::write32)));
 }
@@ -32,14 +35,14 @@ void aesthedes2_vme_crtc_device::device_add_mconfig(machine_config &config)
 u32 aesthedes2_vme_crtc_device::read32(address_space &space, offs_t offset, u32 mem_mask)
 {
     // if (!machine().side_effects_disabled())
-    //     logerror("read @%08x mask=%08x\n", 0x04ffa000U + (offset << 2), mem_mask);
+    //     logerror("read @%08x mask=%08x\n", m_base_addr + (offset << 2), mem_mask);
 
     if (ACCESSING_BITS_16_23) {
         return m_crtc->status_r();
     } else if (ACCESSING_BITS_0_7) {
         return m_crtc->register_r();
     } else {
-        logerror("unknown read @%08x mask=%08x\n", 0x04ffa000U + (offset << 2), mem_mask);
+        logerror("unknown read @%08x mask=%08x\n", m_base_addr + (offset << 2), mem_mask);
         return 0;
     }
 }
@@ -47,13 +50,13 @@ u32 aesthedes2_vme_crtc_device::read32(address_space &space, offs_t offset, u32 
 void aesthedes2_vme_crtc_device::write32(address_space &space, offs_t offset, u32 data, u32 mem_mask)
 {
     // if (!machine().side_effects_disabled())
-    //     logerror("write @%08x mask=%08x data=%08x\n", 0x04ffa000U + (offset << 2), mem_mask, data);
+    //     logerror("write @%08x mask=%08x data=%08x\n", m_base_addr + (offset << 2), mem_mask, data);
 
     if (ACCESSING_BITS_16_23) {
         m_crtc->address_w((u8)(data >> 16));
     } else if (ACCESSING_BITS_0_7) {
         m_crtc->register_w((u8)data);
     } else {
-        logerror("unknown write @%08x mask=%08x data=%08x\n", 0x04ffa000U + (offset << 2), mem_mask, data);
+        logerror("unknown write @%08x mask=%08x data=%08x\n", m_base_addr + (offset << 2), mem_mask, data);
     }
 }
