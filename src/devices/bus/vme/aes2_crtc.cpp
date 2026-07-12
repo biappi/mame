2b037472ef347b57004709092ee1652e8f01b3d5
Enrico Gueli <enrico.gueli@gmail.com>
2026-07-12 19:18:11 +0200

    use logging flags

--- src/devices/bus/vme/aes2_crtc.cpp
+++ src/devices/bus/vme/aes2_crtc.cpp
@@ -1,5 +1,15 @@
 #include "aes2_crtc.h"
 
+#define LOG_FAIL    (1U << 1)
+#define LOG_REGS    (1U << 2)
+
+#define VERBOSE (LOG_FAIL)
+
+#include "logmacro.h"
+
+#define LOGFAIL(...)    LOGMASKED(LOG_FAIL,  __VA_ARGS__)
+#define LOGREGS(...)    LOGMASKED(LOG_REGS,  __VA_ARGS__)
+
 DEFINE_DEVICE_TYPE(VME_AESTHEDES2_CRTC, aesthedes2_vme_crtc_device, "aesthedes2_crtc", "Aesthedes2 VME CRTC (AES C100/0009)");
 
 void aesthedes2_vme_crtc_device::device_start()
@@ -35,30 +45,28 @@ void aesthedes2_vme_crtc_device::device_add_mconfig(machine_config &config)
 
 u32 aesthedes2_vme_crtc_device::read32(address_space &space, offs_t offset, u32 mem_mask)
 {
-    // if (!machine().side_effects_disabled())
-    //     logerror("read @%08x mask=%08x\n", m_base_addr + (offset << 2), mem_mask);
+    LOGREGS("%s read @%08x mask=%08x\n", machine().describe_context(), m_base_addr + (offset << 2), mem_mask);
 
     if (ACCESSING_BITS_16_23) {
         return ((u32)m_crtc->status_r()) << 16;
     } else if (ACCESSING_BITS_0_7) {
         return m_crtc->register_r();
     } else {
-        logerror("unknown read @%08x mask=%08x\n", m_base_addr + (offset << 2), mem_mask);
+        LOGFAIL("unknown read @%08x mask=%08x\n", m_base_addr + (offset << 2), mem_mask);
         return 0;
     }
 }
 
 void aesthedes2_vme_crtc_device::write32(address_space &space, offs_t offset, u32 data, u32 mem_mask)
 {
-    // if (!machine().side_effects_disabled())
-    //     logerror("write @%08x mask=%08x data=%08x\n", m_base_addr + (offset << 2), mem_mask, data);
+    LOGREGS("%s write @%08x mask=%08x data=%08x\n", machine().describe_context(), m_base_addr + (offset << 2), mem_mask, data);
 
     if (ACCESSING_BITS_16_23) {
         m_crtc->address_w((u8)(data >> 16));
     } else if (ACCESSING_BITS_0_7) {
         m_crtc->register_w((u8)data);
     } else {
-        logerror("unknown write @%08x mask=%08x data=%08x\n", m_base_addr + (offset << 2), mem_mask, data);
+        LOGFAIL("unknown write @%08x mask=%08x data=%08x\n", m_base_addr + (offset << 2), mem_mask, data);
     }
 }
 
