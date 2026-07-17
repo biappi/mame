f0eed0ae06375da65484a933b32a436498c12597
Antonio Malara <antonio.malara@getbux.com>
2026-07-17 18:47:37 +0200

    Add crate 3 with a single pme board

--- src/mame/claessens/aesthedes2.cpp
+++ src/mame/claessens/aesthedes2.cpp
@@ -10,6 +10,16 @@
 
 namespace {
 
+
+static DEVICE_INPUT_DEFAULTS_START(terminal_302)
+    DEVICE_INPUT_DEFAULTS( "RS232_TXBAUD", 0xff, RS232_BAUD_19200 )
+    DEVICE_INPUT_DEFAULTS( "RS232_RXBAUD", 0xff, RS232_BAUD_19200 )
+    DEVICE_INPUT_DEFAULTS( "RS232_DATABITS", 0xff, RS232_DATABITS_8 )
+    DEVICE_INPUT_DEFAULTS( "RS232_PARITY", 0xff, RS232_PARITY_NONE )
+    DEVICE_INPUT_DEFAULTS( "RS232_STOPBITS", 0xff, RS232_STOPBITS_1 )
+DEVICE_INPUT_DEFAULTS_END
+
+
 static DEVICE_INPUT_DEFAULTS_START(terminal_504)
     DEVICE_INPUT_DEFAULTS( "RS232_TXBAUD", 0xff, RS232_BAUD_19200 )
     DEVICE_INPUT_DEFAULTS( "RS232_RXBAUD", 0xff, RS232_BAUD_19200 )
@@ -24,14 +34,30 @@ class aesthedes2_state : public driver_device
 public:
     aesthedes2_state(const machine_config &mconfig, device_type type, const char *tag)
         : driver_device(mconfig, type, tag)
+        , m_rs232_302(*this, "rs232_302")
         , m_rs232_504(*this, "rs232_504")
     {
     }
 
     void aesthedes2(machine_config &config)
     {
+        RS232_PORT(config, m_rs232_302, default_rs232_devices, "terminal");
         RS232_PORT(config, m_rs232_504, default_rs232_devices, "terminal");
-        
+
+        // Crate 3
+
+        VME(config, "crate3");
+        VME_SLOT(config, "crate3:02").option_set("pme6822", VME_PME6822).machine_config([this](device_t *dev) {
+            auto &card = downcast<vme_pme6822_card_device &>(*dev);
+            card.set_eprom_regions(":os9kernel", ":ae_config_302");
+            card.rs232_tx_cb().set(m_rs232_302, FUNC(rs232_port_device::write_txd));
+        });
+
+        m_rs232_302->rxd_handler().set("crate3:02:pme6822", FUNC(vme_pme6822_card_device::rs232_rxd_w));
+        m_rs232_302->set_option_device_input_defaults("terminal", DEVICE_INPUT_DEFAULTS_NAME(terminal_302));
+
+        // Crate 5
+
         VME(config, "crate5");
         VME_SLOT(config, "crate5:04").option_set("pme6822", VME_PME6822).machine_config([this](device_t *dev) {
             auto &card = downcast<vme_pme6822_card_device &>(*dev);
@@ -53,7 +79,7 @@ public:
             auto &card = downcast<aesthedes2_vme_crtc_device &>(*dev);
             card.set_base_address(0x04ffa040);
         });
-        
+
         m_rs232_504->rxd_handler().set("crate5:04:pme6822", FUNC(vme_pme6822_card_device::rs232_rxd_w));
         m_rs232_504->set_option_device_input_defaults("terminal", DEVICE_INPUT_DEFAULTS_NAME(terminal_504));
 
@@ -61,6 +87,7 @@ public:
     }
 
 private:
+    required_device<rs232_port_device> m_rs232_302;
     required_device<rs232_port_device> m_rs232_504;
 };
 
