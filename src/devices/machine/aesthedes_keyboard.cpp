8633d045e42a225c4742497e1ef4d6b8a08ec8b5
Enrico Gueli <enrico.gueli@gmail.com>
2026-08-22 11:28:56 +0200

    replace timed injection with TCP server listening to keycodes

--- src/devices/machine/aesthedes_keyboard.cpp
+++ src/devices/machine/aesthedes_keyboard.cpp
@@ -1,14 +1,121 @@
 #include "aesthedes_keyboard.h"
-#include "aesthedes_keycodes.h"
+
+#include "asio.h"
 
 #define VERBOSE (1)
 #include "logmacro.h"
 
+#include <charconv>
+#include <istream>
+
 DEFINE_DEVICE_TYPE(AES2_KEYBOARD, aesthedes_keyboard_device, "aes2_keyboard", "Aesthedes 2 Keyboard")
 
-#define DO_FAKE_KEYSTROKES 1
+class aesthedes_keyboard_server
+{
+public:
+    aesthedes_keyboard_server(aesthedes_keyboard_device &keyboard)
+        : m_keyboard(keyboard)
+        , m_acceptor(m_io_context, asio::ip::tcp::endpoint(asio::ip::address::from_string("127.0.0.1"), 6822))
+    {
+        accept_client();
+        m_thread = std::thread([this] { m_io_context.run(); });
+    }
 
-const size_t table_size = sizeof(keycode_table) / sizeof(uint16_t);
+    ~aesthedes_keyboard_server()
+    {
+        m_io_context.stop();
+        if (m_thread.joinable())
+            m_thread.join();
+    }
+
+private:
+    void accept_client()
+    {
+        m_acceptor.async_accept([this](std::error_code error, asio::ip::tcp::socket socket)
+        {
+            if (!error)
+            {
+                auto client = std::make_shared<client_session>(m_keyboard, std::move(socket));
+                client->start();
+            }
+
+            if (!m_io_context.stopped())
+                accept_client();
+        });
+    }
+
+    class client_session : public std::enable_shared_from_this<client_session>
+    {
+    public:
+        client_session(aesthedes_keyboard_device &keyboard, asio::ip::tcp::socket socket)
+            : m_keyboard(keyboard), m_socket(std::move(socket))
+        {
+        }
+
+        void start()
+        {
+            static constexpr char greeting[] = "Aesthedes 2 emulator - keycodes injector\n";
+            auto self = shared_from_this();
+            asio::async_write(m_socket, asio::buffer(greeting, sizeof(greeting) - 1),
+                [self](std::error_code error, std::size_t)
+                {
+                    if (!error)
+                        self->read_line();
+                });
+        }
+
+    private:
+        void read_line()
+        {
+            auto self = shared_from_this();
+            asio::async_read_until(m_socket, m_input, '\n',
+                [self](std::error_code error, std::size_t)
+                {
+                    if (error)
+                        return;
+
+                    std::string line;
+                    std::istream input_stream(&self->m_input);
+                    std::getline(input_stream, line);
+                    if (!line.empty() && line.back() == '\r')
+                        line.pop_back();
+
+                    uint32_t value = 0;
+                    const auto result = std::from_chars(line.data(), line.data() + line.size(), value);
+                    const bool valid = result.ec == std::errc() && result.ptr == line.data() + line.size() && value <= 0xffff;
+                    if (valid)
+                    {
+                        self->m_keyboard.machine().scheduler().synchronize(
+                            timer_expired_delegate(FUNC(aesthedes_keyboard_device::inject_keycode), &self->m_keyboard), value);
+                    }
+
+                    self->write_response(valid ? "OK\n" : "NOPE\n");
+                });
+        }
+
+        void write_response(const char *response)
+        {
+            auto self = shared_from_this();
+            asio::async_write(m_socket, asio::buffer(response, std::strlen(response)),
+                [self](std::error_code error, std::size_t)
+                {
+                    if (!error)
+                        self->read_line();
+                });
+        }
+
+        aesthedes_keyboard_device &m_keyboard;
+        asio::ip::tcp::socket m_socket;
+        asio::streambuf m_input;
+    };
+
+    aesthedes_keyboard_device &m_keyboard;
+    asio::io_context m_io_context;
+    asio::ip::tcp::acceptor m_acceptor;
+    std::thread m_thread;
+};
+
+aesthedes_keyboard_device::~aesthedes_keyboard_device() = default;
 
 aesthedes_keyboard_device::aesthedes_keyboard_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
 	: device_t(mconfig, AES2_KEYBOARD, tag, owner, clock)
@@ -20,7 +127,7 @@ aesthedes_keyboard_device::aesthedes_keyboard_device(const machine_config &mconf
 
 void aesthedes_keyboard_device::device_start()
 {
-    m_fake_keystrokes_timer = timer_alloc(FUNC(aesthedes_keyboard_device::fake_keystrokes), this);
+	m_server = std::make_unique<aesthedes_keyboard_server>(*this);
 }
 
 void aesthedes_keyboard_device::device_reset()
@@ -28,26 +135,20 @@ void aesthedes_keyboard_device::device_reset()
     m_out_a_port_func(0x0f);
     m_out_b_port_func(0xff);
     m_out_a_strobe_func(1);
-#if DO_FAKE_KEYSTROKES
-    m_fake_keystrokes_timer->adjust(attotime::from_msec(29000), 0, attotime::from_msec(1500));
-#endif
-    m_fake_keystrokes_count = 0;
 }
 
-TIMER_CALLBACK_MEMBER(aesthedes_keyboard_device::fake_keystrokes)
+void aesthedes_keyboard_device::device_stop()
 {
-    u16 keycode = keycode_table[m_fake_keystrokes_count];
-    LOG("sending keycode %d (%d/%d)\n", keycode, m_fake_keystrokes_count, table_size);
+    m_server.reset();
+}
 
+void aesthedes_keyboard_device::inject_keycode(int keycode)
+{
+    LOG("sending keycode %d\n", keycode);
     m_out_a_port_func((u8)(keycode >> 8));
     m_out_b_port_func((u8)(keycode & 0xff));
     m_out_a_strobe_func(0);
     m_out_a_strobe_func(1);
-
-    m_fake_keystrokes_count++;
-    if (m_fake_keystrokes_count >= table_size) {
-        m_fake_keystrokes_timer->enable(false);
-    }
 }
 
 void aesthedes_keyboard_device::leds_pa_w(u8 data) {
--- src/devices/machine/aesthedes_keyboard.h
+++ src/devices/machine/aesthedes_keyboard.h
@@ -5,11 +5,15 @@
 
 #include "emu.h"
 
+#include <memory>
+
+class aesthedes_keyboard_server;
 
 class aesthedes_keyboard_device : public device_t
 {
 public:
 	aesthedes_keyboard_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock = 0);
+    ~aesthedes_keyboard_device();
 
     auto porta_strobe_cb() { return m_out_a_strobe_func.bind(); }
     auto porta_cb() { return m_out_a_port_func.bind(); }
@@ -18,18 +22,18 @@ public:
     void leds_pa_w(u8 data);
     void leds_pb_w(u8 data);
     void leds_x1_w(int state);
+    void inject_keycode(int keycode);
 
 	virtual void device_start() override ATTR_COLD;
 	virtual void device_reset() override ATTR_COLD;
+	virtual void device_stop() override ATTR_COLD;
 
 private:
     devcb_write_line m_out_a_strobe_func;
     devcb_write8 m_out_a_port_func;
     devcb_write8 m_out_b_port_func;
 
-    TIMER_CALLBACK_MEMBER(fake_keystrokes);
-    emu_timer *m_fake_keystrokes_timer;
-    int m_fake_keystrokes_count;
+    std::unique_ptr<aesthedes_keyboard_server> m_server;
     u16 m_leds_latch;
 };
 
